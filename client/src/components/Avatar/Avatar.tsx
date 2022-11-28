c7ea1eb296cc0bf56695aa270ec926afc60285d3
unknown <pitometsk@gmail.com>
2022-11-29 00:07:27 +0200

    Maked btn for upload files. Without saving

--- client/src/components/Avatar/Avatar.tsx
+++ client/src/components/Avatar/Avatar.tsx
@@ -1,20 +1,31 @@
 import React, { FC } from 'react'
 // ==== Assets ====
-import invoker from '../../assets/imgs/avatar.jpg'
+import initialAvatar from '../../assets/imgs/avatar.jpg'
 // ==== Styles ====
 import cl from './Avatar.module.scss'
 import cn from 'classnames'
-
+// ==== Components ====
+import FileUploadBtn from '../UI/FileUploadBtn'
 
 interface IAvatar {
 	photoUrl: string
 	name: string
+	canChangeAvatar?: boolean
 }
 
-const Avatar: FC<IAvatar> = ({ photoUrl, name }) => {
+const Avatar: FC<IAvatar> = ({ photoUrl, name, canChangeAvatar = false }) => {
 	return (
 		<div className={cn(cl.avatar, cl.avatar_marg)}>
-			<img className={cl.avatar__photo} src={photoUrl} alt={name} />
+			<img
+				className={cl.avatar__photo}
+				src={photoUrl || initialAvatar}
+				alt={name}
+			/>
+			{canChangeAvatar ? (
+				<div className={cl['btn-wrapper']}>
+					<FileUploadBtn inputId='user-avatar-upload' />
+				</div>
+			) : null}
 		</div>
 	)
 }
