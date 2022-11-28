3c0c10c06de734d45aa57ef7a2e267589416599a
unknown <pitometsk@gmail.com>
2022-11-29 00:24:30 +0200

    Changed name of component Authorization --> AuthorizationForm

--- client/src/router/mainRouter.ts
+++ client/src/router/mainRouter.ts
@@ -4,12 +4,12 @@ import Information from '../Pages/Information'
 import HighScore from '../Pages/HighScorePage'
 import Settings from '../Pages/Settings'
 import UserProfile from '../components/UserProfile'
-import Authorization from '../components/Authorization'
+import AuthorizationForm from '../components/AuthorizationForm'
 
 export const routes: IRoutes[] = [
 	{ id: 'information-route', path: RouteEnum.information, Page: Information },
 	{ id: 'high-score-route', path: RouteEnum['high-score'], Page: HighScore },
 	{ id: 'settings-route', path: RouteEnum.settings_id, Page: Settings },
 	{ id: 'user-route', path: RouteEnum.user, Page: UserProfile },
-	{ id: 'user-auth', path: RouteEnum.auth_id, Page: Authorization },
+	{ id: 'user-auth', path: RouteEnum.auth_id, Page: AuthorizationForm },
 ]
