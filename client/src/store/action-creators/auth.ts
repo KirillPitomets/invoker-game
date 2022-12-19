dad411d3578f9169139197603dcbc679d0b35fe2
unknown <pitometsk@gmail.com>
2022-12-20 01:23:10 +0200

    Made a logout function for dispatch

--- client/src/store/action-creators/auth.ts
+++ client/src/store/action-creators/auth.ts
@@ -8,6 +8,7 @@ import {
 	registrationPayload,
 
 } from '../../types/reducers/authReducer'
+import { type } from 'os';
 
 export const setStatusLoading = (payload: setStatusLoadingPayload): IAction => {
 	return { type: authActionTypes.SET_STATUS_LOADING, payload }
@@ -25,6 +26,10 @@ export const registration = (payload: registrationPayload): IAction => {
 	return { type: authActionTypes.REGISTRATION, payload }
 }
 
+export const logout = (): IAction => {
+	return { type: authActionTypes.LOGOUT }
+}
+
 export const checkAuth = (): IAction => {
 	return { type: authActionTypes.CHECK_AUTH}
 }
@@ -33,3 +38,6 @@ export const setUserData = (payload: IUser): IAction => {
 	return { type: authActionTypes.SET_USER_DATA, payload }
 }
 
+export const removeUserData = (): IAction => {
+	return { type: authActionTypes.REMOVE_USER_DATA}
+}
