8fb4575db35b6a5a8e1bbd4d2fad087e3c750698
Kirill Pitomets <pitometsk@gmail.com>
2022-12-27 18:42:10 +0200

    Changed inputs

--- client/src/components/EditableDataRow/EditableDataRow.tsx
+++ client/src/components/EditableDataRow/EditableDataRow.tsx
@@ -1,52 +1,46 @@
-import React, { FC, useState } from 'react'
+import React, { FC, ReactElement, useState } from 'react'
 // ==== Styles ====
 import cl from './EditableDataRow.module.scss'
 import cn from 'classnames'
 // ==== Components ====
 import Title from '../UI/Title'
 import Button from '../UI/Button'
-import ButtonEye from '../UI/ButtonEye'
-import EditableDataInput from '../UI/EditableDataInput'
+import PasswordField from '../PasswordField'
+import TextField from '../TextField'
+import ErrorMessage from '../UI/ErrorMessage'
 
 interface IEditableDataRow {
 	title: string
-	initialText: string
 	isPasswordInput?: boolean
-	submit: (args?: any) => void
+	onSubmit?: (args?: any) => void
+	registerInForm?: any
+	errors?: string[]
+	cancelFunction?: (args?: any) => void
 }
 
 const EditableDataRow: FC<IEditableDataRow> = ({
 	title,
-	submit,
 	isPasswordInput = false,
-	initialText,
+	onSubmit,
+	registerInForm,
+	cancelFunction,
+	errors,
+	...props
 }) => {
-	const [inputValue, setInputValue] = useState(initialText)
-
 	const [isChanging, setIsChanging] = useState(false)
-	const [isShowPassword, setIsShowPassword] = useState(false)
-
-	const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setInputValue(e.target.value)
-	}
-
-	const togglePasswordVisibly = () => {
-		setIsShowPassword(prev => !prev)
-	}
-
-	const handlerIsChanging = () => {
+	const handlerIsChanging = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault()
 		setIsChanging(prev => !prev)
 	}
 
 	const cancelChanging = () => {
-		setInputValue(initialText)
 		setIsChanging(false)
+
+		if (cancelFunction) cancelFunction()
 	}
 
-	const saveChangin = () => {
+	const doneChangin = () => {
 		setIsChanging(false)
-
-		// TODO: Make request on server for change username
 	}
 
 	return (
@@ -54,29 +48,28 @@ const EditableDataRow: FC<IEditableDataRow> = ({
 			<Title className={cl.title_marg}>{title}</Title>
 
 			{isPasswordInput ? (
-				<div className={cl['input-wrapper']}>
-					<EditableDataInput
-						value={inputValue}
-						onChange={onChange}
-						isShowText={isShowPassword}
-					/>
-					<ButtonEye
-						isShow={isShowPassword}
-						handleShow={togglePasswordVisibly}
-					/>
-				</div>
+				<PasswordField
+					withIcon={false}
+					registerInForm={registerInForm}
+					disabled={!isChanging}
+				/>
 			) : (
-				<EditableDataInput
-					value={inputValue}
-					onChange={onChange}
-					isShowText={true}
+				<TextField
+					type='text'
+					withIcon={false}
+					registerInForm={registerInForm}
+					disabled={!isChanging}
 				/>
 			)}
+
+			{errors ? <ErrorMessage errors={errors} /> : null}
 
 			<div className={cn(cl.btns, cl.btns_marg)}>
 				{isChanging ? (
 					<>
-						<Button onClick={saveChangin}>Done</Button>
+						<Button onClick={doneChangin} type='submit'>
+							Done
+						</Button>
 						<Button onClick={cancelChanging}>Cancel</Button>
 					</>
 				) : (
