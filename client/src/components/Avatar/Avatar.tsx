43e2869d4c0371908f4e96da26be7c190d291c9a
Kirill Pitomets <pitometsk@gmail.com>
2022-12-27 18:34:11 +0200

    Added cahnge avatar

--- client/src/components/Avatar/Avatar.tsx
+++ client/src/components/Avatar/Avatar.tsx
@@ -1,11 +1,19 @@
-import React, { FC } from 'react'
+import React, { ChangeEvent, FC, useState } from 'react'
+// ==== React hook form ====
+import { useForm } from 'react-hook-form'
+// ==== Types ====
+import { IChangeAvatar } from '../../types/services/UserService'
 // ==== Assets ====
 import initialAvatar from '../../assets/imgs/avatar.jpg'
+// ==== Service ====
+import { UserService } from '../../Services/UserService'
 // ==== Styles ====
 import cl from './Avatar.module.scss'
 import cn from 'classnames'
 // ==== Components ====
 import FileUploadBtn from '../UI/FileUploadBtn'
+import { CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
 
 interface IAvatar {
 	photoUrl: string
@@ -14,6 +22,16 @@ interface IAvatar {
 }
 
 const Avatar: FC<IAvatar> = ({ photoUrl, name, canChangeAvatar = false }) => {
+	const { register } = useForm<IChangeAvatar>()
+
+	const sendFile = (e: ChangeEvent<HTMLInputElement>) => {
+		const file = e.target.files![0]
+
+		UserService.uploadAvatar({
+			avatar: file,
+		})
+	}
+
 	return (
 		<div className={cn(cl.avatar, cl.avatar_marg)}>
 			<img
@@ -21,10 +39,19 @@ const Avatar: FC<IAvatar> = ({ photoUrl, name, canChangeAvatar = false }) => {
 				src={photoUrl || initialAvatar}
 				alt={name}
 			/>
+
 			{canChangeAvatar ? (
-				<div className={cl['btn-wrapper']}>
-					<FileUploadBtn inputId='user-avatar-upload' />
-				</div>
+				<>
+					<div className={cl['change-avatar-btn']}>
+						<FileUploadBtn
+							acceptFiles='.png, .jpeg, .jpg'
+							inputId='user-avatar-upload'
+							text='Change Avatar'
+							registrationInForm={register('avatar')}
+							onChange={sendFile}
+						/>
+					</div>
+				</>
 			) : null}
 		</div>
 	)
