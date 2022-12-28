066abdd23b19e5d55a040adb730bb9fa8ae4c955
Kirill Pitomets <pitometsk@gmail.com>
2022-12-28 19:57:30 +0200

    Moved the UserService type in the blobal types folder. Made functions for change Username, password and upload avatar

--- client/src/Services/UserService.ts
+++ client/src/Services/UserService.ts
@@ -1,9 +1,17 @@
+import { IUploadAvatar } from './../types/services/UserService'
 import axios, { AxiosResponse } from 'axios'
 import { API_URL } from './../http/index'
 import $api from '../http'
 // ==== Types ====
 import { IAuthResponse } from '../models/response/AuthResponse'
-import { ILogin, IRegistration } from './types/UserService'
+import {
+	IChangeUsername,
+	IChangeUserPassword,
+	ILogin,
+	IRegistration,
+} from '../types/services/UserService'
+import { IUser } from '../models/IUser'
+import { IFile } from '../models/IFile'
 
 export class UserService {
 	static async login({
@@ -25,11 +33,35 @@ export class UserService {
 		})
 	}
 
+	static async changePassword({
+		password,
+	}: IChangeUserPassword): Promise<void> {
+		return $api.put('/password', { password })
+	}
+
+	static async changeUsername({
+		username,
+	}: IChangeUsername): Promise<AxiosResponse<IUser>> {
+		return $api.put('/username', { username })
+	}
+
 	static async checkAuth() {
 		return axios.get(`${API_URL}/refresh`, { withCredentials: true })
 	}
 
 	static async logout() {
 		return $api.post('/logout')
+	}
+
+	static async uploadAvatar({
+		avatar,
+	}: IUploadAvatar): Promise<AxiosResponse<IUser>> {
+		
+		const formData = new FormData()
+		formData.append('avatar', avatar)
+		
+		return $api.put('/avatar', formData, {
+			headers: { "Content-Type": "multipart/form-data" },
+		})
 	}
 }
