1e5730f01823c862f1a7db38212cdf214650ec67
Kirill Pitomets <pitometsk@gmail.com>
2022-12-28 22:55:40 +0200

    Added functions to change username and password

--- client/src/components/UserProfile/UserProfile.tsx
+++ client/src/components/UserProfile/UserProfile.tsx
@@ -1,23 +1,34 @@
 import React, { useEffect } from 'react'
+// ==== React hook form ====
+import { useForm, SubmitHandler } from 'react-hook-form'
 // ==== Redux ====
 import { useDispatch } from 'react-redux'
 import { useTypedSelector } from '../../hooks/useTypedSelector'
-import { logout } from '../../store/action-creators/auth'
+import {
+	changePassword,
+	changeUsername,
+	logout,
+} from '../../store/action-creators/user'
 import { hideMainPopUp } from '../../store/action-creators/popup'
 // ==== React router dom  ====
 import { useNavigate } from 'react-router-dom'
 // ==== Types ====
 import { RouteEnum } from '../../types/route'
+import {
+	IChangeUsername,
+	IChangeUserPassword,
+} from '../../types/services/UserService'
 // ==== Styles ====
 import cl from './UserProfile.module.scss'
 // ==== Components ====
 import ErrorMessage from '../UI/ErrorMessage'
 import Avatar from '../Avatar'
-import EditableDataRow from '../EditableDataRow'
 import Button from '../UI/Button'
+import ChangeUsernameForm from '../ChangeUsernameForm'
+import ChangeUserPasswordForm from '../ChangeUserPasswordForm'
 
 const UserProfile = () => {
-	const { isAuth, user } = useTypedSelector(state => state.auth)
+	const { isAuth, user } = useTypedSelector(state => state.user)
 	const { isServerWorking } = useTypedSelector(state => state.error)
 
 	const dispatch = useDispatch()
@@ -25,8 +36,20 @@ const UserProfile = () => {
 
 	const logoutUser = () => {
 		dispatch(logout())
-		navigate(`/`)
-		dispatch( hideMainPopUp())
+		navigate('/')
+		dispatch(hideMainPopUp())
+	}
+
+	// ==== Change username ====
+	// =========================
+	const onSubmitUsername: SubmitHandler<IChangeUsername> = data => {
+		dispatch(changeUsername(data))
+	}
+	
+	// ==== Change password ====
+	// =========================
+	const onSubmitPassword: SubmitHandler<IChangeUserPassword> = data => {
+		dispatch(changePassword(data))
 	}
 
 	useEffect(() => {
@@ -37,28 +60,22 @@ const UserProfile = () => {
 		return <ErrorMessage errors={['Server is not working now. Try later']} />
 
 	return (
-		<div className={cl.wrapper}>
-			<Avatar
-				photoUrl={user.avatar}
-				name={user.username}
-				canChangeAvatar={true}
-			/>
-
-			<div className={cl.inner}>
-				<EditableDataRow
-					title='Username'
-					initialText={user.username}
-					submit={text => console.log(text)}
+		<div className={cl.container}>
+			<div className={cl.wrapper}>
+				<Avatar
+					photoUrl={user.avatar}
+					name={user.username}
+					canChangeAvatar={true}
 				/>
 
-				<EditableDataRow
-					title='Password'
-					initialText='This your password :3'
-					isPasswordInput={true}
-					submit={text => console.log(text)}
-				/>
+				<div className={cl.inner}>
+					{/* Change username */}
+					<ChangeUsernameForm onSubmit={onSubmitUsername} />
+
+					{/* Change password */}
+					<ChangeUserPasswordForm onSubmit={onSubmitPassword} />
+				</div>
 			</div>
-
 			<Button onClick={logoutUser}>Logout</Button>
 		</div>
 	)
