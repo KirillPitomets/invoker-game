2c437d2b9179ef6e2d583d009db15a75c2073f51
Kirill Pitomets <pitometsk@gmail.com>
2022-12-28 22:53:20 +0200

    Added types for props and changed structure of the returned element

--- client/src/components/UI/FileUploadBtn/FileUploadBtn.tsx
+++ client/src/components/UI/FileUploadBtn/FileUploadBtn.tsx
@@ -1,16 +1,32 @@
-import React, { FC } from 'react'
+import React, { FC, InputHTMLAttributes } from 'react'
 // ==== Styles ====
 import cl from './FileUploadBtn.module.scss'
 
-interface IFileUploadBtn {
+interface IFileUploadBtn extends InputHTMLAttributes<HTMLInputElement> {
 	inputId: string
+	text: string
+	registrationInForm?: any
+	acceptFiles: string
 }
 
-const FileUploadBtn: FC<IFileUploadBtn> = ({ inputId}) => {
+const FileUploadBtn: FC<IFileUploadBtn> = ({
+	inputId,
+	text,
+	acceptFiles,
+	registrationInForm,
+	...props
+}) => {
 	return (
-		<label className={cl.label}  htmlFor={inputId}>
-			<p>Change avatar</p>
-			<input className={cl.input} id={inputId} />
+		<label className={cl.label} htmlFor={inputId}>
+			<p>{text}</p>
+			<input
+				className={cl.input}
+				id={inputId}
+				type='file'
+				accept={acceptFiles}
+				{...registrationInForm}
+				{...props}
+			/>
 		</label>
 	)
 }
